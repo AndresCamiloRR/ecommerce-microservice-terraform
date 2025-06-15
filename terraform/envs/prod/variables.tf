658f6b6c2dc3931d1644ee26c41262b9bba450fe
AndresCamiloRR <101947895+AndresCamiloRR@users.noreply.github.com>
2025-06-15 15:46:03 -0500

    refactor: change node_count for a faster deployment

--- terraform/envs/prod/variables.tf
+++ terraform/envs/prod/variables.tf
@@ -2,6 +2,6 @@ variable "resource_group_name" { default = "rg-prod" }
 variable "location"            { default = "eastus" }
 variable "aks_cluster_name"    { default = "aks-prod" }
 variable "dns_prefix"          { default = "prodaks" }
-variable "node_count"          { default = 1 }
+variable "node_count"          { default = 3 }
 variable "vm_size"             { default = "Standard_DS2_v2" }
 variable "environment"         { default = "prod" }
--- terraform/envs/stage/variables.tf
+++ terraform/envs/stage/variables.tf
@@ -2,6 +2,6 @@ variable "resource_group_name" { default = "rg-stage" }
 variable "location"            { default = "eastus" }
 variable "aks_cluster_name"    { default = "aks-stage" }
 variable "dns_prefix"          { default = "stageaks" }
-variable "node_count"          { default = 1 }
+variable "node_count"          { default = 2 }
 variable "vm_size"             { default = "Standard_DS2_v2" }
 variable "environment"         { default = "stage" }
